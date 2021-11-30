424b515e47b71694c129520bd783a1593da63d45
Bill <sivm78@mail.ru>
2021-11-30 20:55:20 +0400

    secondCommit

--- app/src/main/java/example/bill/mylesson3/MainActivity.kt
+++ app/src/main/java/example/bill/mylesson3/MainActivity.kt
@@ -1,6 +1,7 @@
 package example.bill.mylesson3
 
 import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
@@ -19,44 +20,12 @@ class MainActivity : AppCompatActivity() {
         Log.d(TAG, "onCreat")
         nextActivityButton = findViewById(R.id.next_activity_button)
 
-        actionBar?.title = "Second Activity"
+
 
         nextActivityButton.setOnClickListener {
-
-            val secondActivityIntent: Intent = Intent(this, SecondActivity::class.java)
-            secondActivityIntent.putExtra(HELLO_KEY, "Hi from FirstActivity")
-            startActivity(secondActivityIntent)
-
+            val googleLink = Uri.parse("https://google.com")
+            val openBrowserIntent = Intent(Intent.ACTION_VIEW, googleLink)
+            startActivity(openBrowserIntent)
         }
-    }
-
-    override fun onStart() {
-        super.onStart()
-        Log.d(TAG, "onStart")
-    }
-
-    override fun onResume() {
-        super.onResume()
-        Log.d(TAG, "onResume")
-    }
-
-    override fun onPause() {
-        super.onPause()
-        Log.d(TAG, "onPause")
-    }
-
-    override fun onStop() {
-        super.onStop()
-        Log.d(TAG, "onStop")
-    }
-
-    override fun onRestart() {
-        super.onRestart()
-        Log.d(TAG, "onRestart")
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        Log.d(TAG, "onDestroy")
     }
 }
